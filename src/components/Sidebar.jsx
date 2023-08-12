bb8eab6ebe40ccde2055484955a95a21d130dce6
Abin John <abinjohnanto@gmail.com>
2023-08-12 18:26:34 +0530

    feat: sidebar navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,38 @@
+import { NavLink } from 'react-router-dom';
+
+const menuItems = [
+  {
+    label: 'Dashboard',
+    path: '/',
+  },
+  {
+    label: 'Departments',
+    path: '/departments',
+  },
+  {
+    label: 'Products',
+    path: '/products',
+  },
+];
+
+const MenuItem = ({ menuItem }) => {
+  const { label, path } = menuItem;
+
+  return (
+    <NavLink to={path} className="menu-item">
+      <span>{label}</span>
+    </NavLink>
+  );
+};
+
 const Sidebar = () => {
-  return <div className="sidebar"></div>;
+  return (
+    <nav className="sidebar">
+      {menuItems.map((item, id) => (
+        <MenuItem menuItem={item} key={id} />
+      ))}
+    </nav>
+  );
 };
 
 export default Sidebar;
