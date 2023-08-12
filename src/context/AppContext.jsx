dd598961fa594d49a6ac7546de5769d5fadb0a0c
Abin John <abinjohnanto@gmail.com>
2023-08-12 18:31:16 +0530

    feat: import data and set up context

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,9 +1,21 @@
 import { createContext, useContext, useState } from 'react';
+import { inventoryData, LOCAL_STORAGE_KEYS } from '../constants';
 
 const AppContext = createContext({});
 
 const AppProvider = ({ children }) => {
-  const contextValue = {};
+  const [inventory, setInventory] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEYS.INVENTORY)) ??
+      inventoryData
+  );
+
+  const setLocalStorageInventory = (data) => {
+    localStorage.setItem(LOCAL_STORAGE_KEYS.INVENTORY, JSON.stringify(data));
+    setInventory(data);
+  };
+
+  const contextValue = { inventory, setInventory: setLocalStorageInventory };
+
   return (
     <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
   );
